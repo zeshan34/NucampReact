3bc453eb825d8d457bd8f5c0df1a0cb8c0af4a34
shaikhzeshan@gmail.com <shaikhzeshan@gmail.com>
2020-06-05 01:05:57 -0500

    component part 2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Directory from './components/DirectoryComponent';
+import {CAMPSITES} from './shared/campsites';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      campsites: CAMPSITES
+    };
+  }
     render() {
         return (
             <div className="App">
                 <Navbar dark color="primary">
                 <div className="container">
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
-                    <Directory/>
+                    <Directory campsites ={this.state.campsites}/>
                 </div>
                 </Navbar>
             </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
+
 
 ReactDOM.render(
   <React.StrictMode>
